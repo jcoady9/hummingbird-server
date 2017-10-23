3c20121fc3cb6ac34ab089a1b9ef4b023d9134d3
Peter Lejeck <peter.lejeck@gmail.com>
2017-10-23 14:47:10 -0700

    Add User#ao_imported column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013043506) do
+ActiveRecord::Schema.define(version: 20171023210622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1536,6 +1536,7 @@ ActiveRecord::Schema.define(version: 20171013043506) do
     t.string   "ao_password"
     t.string   "ao_facebook_id"
     t.integer  "ao_pro"
+    t.string   "ao_imported"
   end
 
   add_index "users", ["ao_id"], name: "index_users_on_ao_id", unique: true, using: :btree
